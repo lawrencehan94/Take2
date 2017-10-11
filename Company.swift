fac383f5b02ae54a23e4549717296d5a89914dbb
Lawrence <lawrencehan94@gmail.com>
2017-10-11 01:39:15 -0700

    v1

---
+++ Company.swift
@@ -0,0 +1,16 @@
+//
+//  Company.swift
+//  Fin Wiz
+//
+//  Created by Lawrence Han on 10/6/17.
+//  Copyright © 2017 Lawrence Han. All rights reserved.
+//
+
+import UIKit
+
+class Company: NSObject {
+    
+    var ticker: String!
+    var value: String!
+
+}
